65c3dcb88d65a35d53b24ae5b9a1f9934ccb6d40
Gleb1305 <gleb8062@gmail.com>
2020-03-02 18:03:52 +0200

    Added firs task

---
+++ Lecture_1/Task_1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout << " Chislo 1:";
+    cin >> a;
+    cout << " Chislo 2:";
+    cin >> b;
+    cout << "Result: ";
+    cout << a + b;
+}
