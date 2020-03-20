ef51b6dea46654bbcd631cd13ec30d3af7b23c1c
Gleb1305 <gleb8062@gmail.com>
2020-03-20 21:32:49 +0200

    Added task 5

---
+++ Lecture_2/Task_5/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int size = 0;
+    cout << "Vvedite vysotu yelki: ";
+    cin >> size;
+    
+    for (int lineNum = 1; lineNum <= size; lineNum++)
+    {
+        for (int actualSpace = 0; actualSpace < size - lineNum; actualSpace++)
+            cout << " ";
+        for (int actualStart = 0; actualStart < lineNum * 2 - 1; actualStart++)
+            cout << "*";
+        cout << endl;
+        ;
+    }
+    for (int spaseAmount = 1; spaseAmount < size; spaseAmount++)
+        cout << " ";
+    cout << "*";
+}
